ab2668911656046eba446fad915416087542910d
phantom1845 <38790326+phantom1845@users.noreply.github.com>
2022-01-10 20:43:03 +0800

    MESSAGE

--- SPYDER/cogs/Quiz.py
+++ SPYDER/cogs/Quiz.py
@@ -11,18 +11,18 @@ class cognamemod(commands.Cog):
 #body
     #clear messages
     @commands.command()
-    async def clear(self, ctx, amount=10):
+    async def clear1(self, ctx, amount=10):
         await ctx.channel.purge(limit=amount + 1)
         await ctx.send(f'{amount} messages cleared')
 
     #kick member
     @commands.command()
-    async def kick(self, ctx, member : discord.Member, *, reason=None):
+    async def kick1(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
 
     #ban member
     @commands.command()
-    async def ban(self, ctx, member : discord.Member, *, reason=None):
+    async def ban1(self, ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason)
         await ctx.send(f'{member.mention} was banned from the server. Reason: {reason}')
 #/body
